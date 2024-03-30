1bb5de55880f3dd4b1e1b7dfcd2d289f67397ac5
Gabriele Zucchi <19gabrielezucchi99@gmail.com>
2024-03-30 14:23:47 +0100

    added piante fiori and funghi pages with json

---
+++ fiori.js
@@ -0,0 +1,52 @@
+
+    
+    
+    fetch('./plants.json')
+    .then(response => response.json())
+    .then(data => {
+      // Elaborazione dei dati JSON
+      const piante = data.fiori;
+  
+      // Creazione degli elementi HTML e inserimento nel documento
+      let container = document.querySelector('.container');
+  
+      piante.forEach(fiore => {
+        let articolo = document.createElement('article');
+/*         let titolo = document.createElement('h2');
+        let descrizione = document.createElement('p');
+ */
+        articolo.innerHTML = `
+        <h2>${fiore.nome}</h2>
+        <p>${fiore.descrizione}</p>`;
+
+        container.appendChild(articolo);
+        articolo.classList.add("creaSezione");
+        
+/*         titolo.textContent = pianta.pianta;
+        descrizione.textContent = pianta.descrizione;
+  
+        articolo.appendChild(titolo);
+        articolo.appendChild(descrizione);
+        container.appendChild(articolo);
+
+ */        console.log(data);
+      });
+    })
+    .catch(error => {
+      console.error('Si è verificato un errore durante il caricamento del file JSON:', error);
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ funghi.js
@@ -0,0 +1,52 @@
+
+    
+    
+    fetch('./plants.json')
+    .then(response => response.json())
+    .then(data => {
+      // Elaborazione dei dati JSON
+      const piante = data.funghi;
+  
+      // Creazione degli elementi HTML e inserimento nel documento
+      let container = document.querySelector('.container');
+  
+      piante.forEach(fungo => {
+        let articolo = document.createElement('article');
+/*         let titolo = document.createElement('h2');
+        let descrizione = document.createElement('p');
+ */
+        articolo.innerHTML = `
+        <h2>${fungo.nome}</h2>
+        <p>${fungo.descrizione}</p>`;
+
+        container.appendChild(articolo);
+        articolo.classList.add("creaSezione");
+        
+/*         titolo.textContent = pianta.pianta;
+        descrizione.textContent = pianta.descrizione;
+  
+        articolo.appendChild(titolo);
+        articolo.appendChild(descrizione);
+        container.appendChild(articolo);
+
+ */        console.log(data);
+      });
+    })
+    .catch(error => {
+      console.error('Si è verificato un errore durante il caricamento del file JSON:', error);
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
